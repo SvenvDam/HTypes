01dceffdad7d4289c1f1a5af4f9c3210f598e498
Sven van Dam <sven.dam@protonmail.com>
2019-09-09 22:58:40 +0200

    Removed integration test config stuff since it doesnt work anyways

--- src/test/scala/com/svenvandam/htypes/BaseHbaseTest.scala
+++ src/test/scala/com/svenvandam/htypes/BaseHbaseTest.scala
@@ -12,13 +12,6 @@ trait BaseHbaseTest extends FunSuiteLike with BeforeAndAfterAll with LazyLogging
 
   override def beforeAll {
     super.beforeAll()
-    System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog")
-    val dir = sys.env.getOrElse("HBASE_TMP_DIR", "/tmp")
-    val testDataDir = s"$dir/embedded-hbase-test-data-${UUID.randomUUID().toString}"
-    logger.info(s"TestDir for hbase test: $testDataDir")
-    System.setProperty(HBaseCommonTestingUtility.BASE_TEST_DIRECTORY_KEY, testDataDir)
-    hBaseUtility.getConfiguration.set("hbase.master.hostname", "localhost")
-    hBaseUtility.getConfiguration.set("hbase.regionserver.hostname", "localhost")
     hBaseUtility.startMiniCluster()
     logger.info("HBase test cluster started.")
   }
