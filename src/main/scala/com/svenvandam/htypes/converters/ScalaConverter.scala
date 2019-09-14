c09a46d39edba21cd2e7359d6931493878364e87
Sven van Dam <sven.dam@protonmail.com>
2019-09-14 12:18:49 +0200

    Restricted converters and brought into single package

--- src/main/scala/com/svenvandam/htypes/converters/ScalaConverter.scala
+++ src/main/scala/com/svenvandam/htypes/converters/ScalaConverter.scala
@@ -1,9 +1,9 @@
 package com.svenvandam.htypes.converters
 
 import org.apache.hadoop.hbase.client.{Result, ResultScanner}
-import scala.collection.JavaConverters._
+import scala.collection.convert._
 
-trait ScalaConverter {
+trait ScalaConverter extends AsScalaExtensions {
   implicit def resultToScala(res: ResultScanner): Iterable[Result] = res.asScala
 }
 
